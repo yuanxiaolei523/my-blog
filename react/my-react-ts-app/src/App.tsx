1c29264b5ee4baf845c525c2ee64d624b5219afd
yxl <yxiaolei.yuan@qunar.com>
2021-04-26 18:51:24 +0800

    ts-react

---
+++ react/my-react-ts-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Hello from './components/Hello'
+
+function App() {
+  return (
+    <div className="App">
+      <Hello name={'zhangsna'}></Hello>
+    </div>
+  );
+}
+
+export default App;
