3ea9b5fe4df771f2f235776415cc8d56b1775483
yxl <yxiaolei.yuan@qunar.com>
2021-06-09 20:05:35 +0800

    new

--- js/8.hand/2-new.js
+++ js/8.hand/2-new.js
@@ -4,6 +4,11 @@
  * 2. 将空对象的原型指向构造函数的原型对象
  * 3. 将this指向这个空对象，执行构造函数中的代码，以获取私有属性
  * 4. 如果构造函数返回了一个对象res，就将该返回值res返回，如果返回值不是对象，就将创建的对象返回
+ * 
+ * new 的特性：
+ * 1. 可以访问构造函数上的属性和方法 通过apply将构造函数上的属性绑定到obj上
+ * 2. 可以访问构造函数原型上的属性和方法 通过原型链的形式让obj可以访问构造函数原型上的属性和方法
+ * 3. new会返回一个新的对象obj，如果构造函数中有返回值并且返回值为对象形式，那么就返回这个对象，否则返回obj
  */
 // function myNew(cons) {
 //     let obj = Object.create(null);
@@ -19,24 +24,31 @@ function Person(name, age) {
 }
 
 Person.prototype.name = '123';
-var person = new2(Person, 'shine', 18)
-console.log(person, person.name);
+Person.prototype.getName = function () {
+    // console.log();
+    return this.age;
+}
+// var person = new2(Person, 'shine', 18)
+// console.log(person, person.name);
 
-function new2() {
-    let obj = new Object();
-    let cons = [].shift.call(arguments); // 新函数
-    console.log(typeof cons);
-    obj.__proto__ = cons.Prototype;
-    var ret = cons.myApply(obj, arguments); // 如果构造函数返回的是对象，那么new会将构造函数返回的对象return，否则返回当前实例
-    return ret instanceof Object ? ret : obj;
+function myNew() {
+    var obj = {};
+    let constructor = Array.prototype.shift.call(arguments);
+    obj.__proto__ = constructor.prototype;
+    var ret = constructor.apply(obj, arguments);
+    return typeof ret === 'object' ? ret : obj;
 }
 
-Function.prototype.myApply = function (ctx) {
-    ctx = ctx || window; // 以为当ctx不传的时候，默认绑定的是window
-    ctx.fn = this;
-    let args = [].slice.call(arguments, 1);
-    let ret = ctx.fn(args);
-    delete ctx.fn;
-    return ret;
+// Function.prototype.myApply = function (ctx) {
+//     ctx = ctx || window; // 以为当ctx不传的时候，默认绑定的是window
+//     ctx.fn = this;
+//     let args = [].slice.call(arguments, 1);
+//     let ret = ctx.fn(args);
+//     delete ctx.fn;
+//     return ret;
+// }
 
-}
+
+// let p = new Person('shine', 18);
+// p.name = '456'
+// console.log(p.age, p.strength, p.getName(), p.name, p.__proto__);
